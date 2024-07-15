d019ab162ede330a1376eaa56214f1af3dfc58f3
SashaMaslak <owmaslak@gmail.com>
2024-07-15 11:37:34 +0300

    completed dev14

--- src/components/Modals/ModalProduct/ModalProduct.jsx
+++ src/components/Modals/ModalProduct/ModalProduct.jsx
@@ -25,6 +25,7 @@ export const ModalProduct = ({
 }) => {
   const [product, setProduct] = useState(productToEdit || emptyForm)
   const { setIsLoaded } = useContext(IsLoadedContext)
+  const [errorsInputs, setErrorsInputs] = useState(emptyForm)
 
   useEffect(() => {
     const handleKeyPress = event => {
@@ -46,13 +47,57 @@ export const ModalProduct = ({
     return null
   }
 
+  const checkError = () => {
+    let hasErrorInput = false
+    const newErrorsInput = emptyForm
+
+    for (const key in product) {
+      if (product[key].trim() === "") {
+        newErrorsInput[key] = `${
+          key.charAt(0).toUpperCase() + key.slice(1)
+        } не може бути пустим.`
+        hasErrorInput = true
+      }
+    }
+
+    setErrorsInputs(newErrorsInput)
+    return hasErrorInput
+  }
+
   const handleChange = (e, inputName) => {
     const value = e.target.value
     setProduct({ ...product, [inputName]: value })
+    if (value !== "") {
+      setErrorsInputs(prevErrors => ({
+        ...prevErrors,
+        [inputName]: "",
+      }))
+    }
+  }
+
+  const handleBlur = inputName => {
+    if (product[inputName].trim() === "") {
+      setErrorsInputs(prevErrors => ({
+        ...prevErrors,
+        [inputName]: `${
+          inputName.charAt(0).toUpperCase() + inputName.slice(1)
+        } is required`,
+      }))
+    } else {
+      setErrorsInputs(prevErrors => ({
+        ...prevErrors,
+        [inputName]: "",
+      }))
+    }
   }
 
   const handleSubmit = async e => {
     e.preventDefault()
+    const isError = checkError()
+
+    if (isError) {
+      return
+    }
     if (e.target.textContent === "Change") await handleEditProduct(product)
     if (e.target.textContent === "Create NEW") await handleCopyProduct(product)
     setIsLoaded(false)
@@ -76,48 +121,83 @@ export const ModalProduct = ({
           <label className={css.labelForm}>Category:</label>
           <Input
             value={product?.category}
+            isEmpty={errorsInputs.category}
             onChange={e => handleChange(e, "category")}
+            onBlur={() => handleBlur("category")}
             className={css.inputForm}
             inputName="categoryProduct"
             inputType="text"
             inputPlaceholder="Category"
             inModal
           />
+          {errorsInputs.category && (
+            <p className={`${css.errorInput} ${css.errorInputCategory}`}>
+              *{errorsInputs.category}
+            </p>
+          )}
           <label className={css.labelForm}>Name:</label>
           <Input
             value={product?.name}
+            isEmpty={errorsInputs.name}
             onChange={e => handleChange(e, "name")}
+            onBlur={() => handleBlur("name")}
             inputName="nameProduct"
             inputType="text"
             inputPlaceholder="Name"
             inModal
           />
+          {errorsInputs.name && (
+            <p className={`${css.errorInput} ${css.errorInputName}`}>
+              *{errorsInputs.name}
+            </p>
+          )}
           <label className={css.labelForm}>Quantity:</label>
           <Input
             value={product?.quantity}
+            isEmpty={errorsInputs.quantity}
             onChange={e => handleChange(e, "quantity")}
+            onBlur={() => handleBlur("quantity")}
             inputName="quantityProduct"
             inputType="number"
             inputPlaceholder="Quantity"
             inModal
           />
+          {errorsInputs.quantity && (
+            <p className={`${css.errorInput} ${css.errorInputQuantity}`}>
+              *{errorsInputs.quantity}
+            </p>
+          )}
           <label className={css.labelForm}>Price:</label>
           <Input
             value={product?.price}
+            isEmpty={errorsInputs.price}
             onChange={e => handleChange(e, "price")}
+            onBlur={() => handleBlur("price")}
             inputName="priceProduct"
             inputType="number"
             inputPlaceholder="Price"
             inModal
           />
+          {errorsInputs.price && (
+            <p className={`${css.errorInput} ${css.errorInputPrice}`}>
+              *{errorsInputs.price}
+            </p>
+          )}
           <label className={css.labelForm}>Description:</label>
           <TextArea
             value={product?.description}
+            isEmpty={errorsInputs.description}
             onChange={e => handleChange(e, "description")}
+            onBlur={() => handleBlur("description")}
             textareaPlaceholder="Description"
             title="Description:"
             height="100px"
           />
+          {errorsInputs.description && (
+            <p className={`${css.errorInput} ${css.errorInputDescription}`}>
+              *{errorsInputs.description}
+            </p>
+          )}
           <div className={css.btnGroup}>
             <Button
               onClick={() => handleCloseModal(false)}
